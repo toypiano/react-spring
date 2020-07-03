7cb0a240a77fc3588ec889f4ef13bb6f800d36ff
Hayoun Lee <sidhlee@gmail.com>
2020-07-03 20:30:21 +0900

    Transition with multiple components(items)

--- src/Toggle.js
+++ src/Toggle.js
@@ -2,28 +2,61 @@ import React, { useState } from 'react';
 import { useTransition, animated } from 'react-spring';
 
 const Toggle = () => {
-  const [isToggled, setIsToggled] = useState(false);
+  const [items, setItems] = useState([
+    {
+      letter: 'H',
+      key: 1,
+    },
+    {
+      letter: 'E',
+      key: 2,
+    },
+    {
+      letter: 'L',
+      key: 3,
+    },
+    {
+      letter: 'L',
+      key: 4,
+    },
+    {
+      letter: 'O',
+      key: 5,
+    },
+  ]);
   // Transition is often used to transition between different components
   // But we're checking if mounting/un-mounting of single component works first
-  const transition = useTransition(isToggled, null, {
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
-  });
+  const transition = useTransition(
+    items /* item(s) to transition */,
+    (item) => item.key /* specify field that has the key value */,
+    {
+      from: { opacity: 0 },
+      enter: { opacity: 1 },
+      leave: { opacity: 0 },
+    }
+  );
 
   return (
     <div>
-      {transition.map(
-        ({ item, key, props }) =>
-          // mount/unmount single-component based on boolean state
-          item && (
-            <animated.h1 key={key} style={props}>
-              Hello
-            </animated.h1>
-          )
-      )}
+      {transition.map(({ item, key, props }) => (
+        // mount/unmount single-component based on boolean state
+        <animated.h1 style={props} key={key}>
+          {item.letter}
+        </animated.h1>
+      ))}
 
-      <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
+      <button
+        onClick={() =>
+          setItems([
+            {
+              letter: 'H',
+              key: 1,
+            },
+          ])
+        }
+      >
+        Toggle
+      </button>
     </div>
   );
 };
