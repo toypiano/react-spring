d6524da13ce1d1585c61ff3964e933a95b54ec67
Hayoun Lee <sidhlee@gmail.com>
2020-07-03 12:21:07 +0900

    6. Interpolation Basics
    You can pass non-css values into useSpring and interpolate them in the style prop passed to animated component

--- src/Toggle.js
+++ src/Toggle.js
@@ -3,19 +3,35 @@ import { useSpring, animated } from 'react-spring';
 
 const Toggle = () => {
   const [isToggled, setIsToggled] = useState(false);
-  const fade = useSpring({
+
+  // useSpring is a basis of all the other hooks in react-spring
+  // so if the passed style object works with useSpring, it will work with other hooks
+
+  // Each passed property is wrapped inside Spring
+  // and has access to the methods like 'interpolate'
+  const { color, y } = useSpring({
     // opacity: isToggled ? 1 : 0,
     color: isToggled ? '#000' : 'green', // can use any values
-    fontSize: isToggled ? '2rem' : '5em', // can use any units
-    transform: isToggled ? 'translate3d(0,0,0,)' : 'translate3d(-100px,0,0)',
+    y: isToggled ? 0 : -50, // we can re-use this 'y' value in different places
   });
 
   return (
     <div>
-      <animated.h1 style={fade}>Hello</animated.h1>
+      <animated.h1
+        style={{
+          color,
+          // View interpolation can be a little faster & take up less space
+          transform: y.interpolate((y) => `translate3d(0, ${y}px, 0)`),
+        }}
+      >
+        Hello
+      </animated.h1>
       <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
     </div>
   );
 };
+
+// You can also re-name the animated component (not used often)
+// const AnimatedTitle = animated(componentName);
 
 export default Toggle;
