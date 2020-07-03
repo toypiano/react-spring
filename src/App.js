f485df036ee2e7500c157cc82bdac73be7b0ebbf
Hayoun Lee <sidhlee@gmail.com>
2020-07-03 10:54:34 +0900

    3. A Basic Spring
    Pass style objects to useSpring and pass the return value to the style prop of animated.div

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import { useSpring, animated } from 'react-spring';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="logo" />
-          <button className="menu-button">Menu</button>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  // You can take the 'to' values out to the top-level
+  // and react-spring will understand that it's the 'to' value.
+  const fade = useSpring({ from: { opacity: 0 }, opacity: 1 });
+
+  return (
+    // animated will take the animation out of the react
+    // giving performance benefits
+    <animated.div className="App" style={fade}>
+      <header className="App-header">
+        <img src={logo} className="logo" alt="logo" />
+        <button className="menu-button">Menu</button>
+      </header>
+    </animated.div>
+  );
+};
 
 export default App;
