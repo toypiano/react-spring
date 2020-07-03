2cc051ea822d1ad6c93c9f0021fd6ed8582aa867
Hayoun Lee <sidhlee@gmail.com>
2020-07-03 20:03:04 +0900

    10. Transition with Mounting/Unmounting
    useTransition(item, key, props) returns an array of item(s) to map into animated component

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,36 +1,28 @@
 import React, { useState } from 'react';
-import { useSpring, animated } from 'react-spring';
+import { useTransition, animated } from 'react-spring';
 
 const Toggle = () => {
   const [isToggled, setIsToggled] = useState(false);
-
-  // useSpring is a basis of all the other hooks in react-spring
-  // so if the passed style object works with useSpring, it will work with other hooks
-
-  // Each passed property is wrapped inside Spring
-  // and has access to the methods like 'interpolate'
-  const { color, y } = useSpring({
-    // opacity: isToggled ? 1 : 0,
-    color: isToggled ? '#000' : 'green', // can use any values
-    y: isToggled ? 1 : 0, // when using 'range/output', this has to be either 0 or 1;
+  // Transition is often used to transition between different components
+  // But we're checking if mounting/un-mounting of single component works first
+  const transition = useTransition(isToggled, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
   });
 
   return (
     <div>
-      <animated.h1
-        style={{
-          color,
-          // View interpolation can be a little faster & take up less space
-          transform: y
-            .interpolate({
-              range: [0, 0.25, 0.5, 0.75, 1], // keyframe %
-              output: [0, -25, -50, -100, -50], // output value
-            })
-            .interpolate((y) => `translate3d(0, ${y}px, 0)`),
-        }}
-      >
-        Hello
-      </animated.h1>
+      {transition.map(
+        ({ item, key, props }) =>
+          // mount/unmount single-component based on boolean state
+          item && (
+            <animated.h1 key={key} style={props}>
+              Hello
+            </animated.h1>
+          )
+      )}
+
       <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
     </div>
   );
