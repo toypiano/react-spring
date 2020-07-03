a7417e2c5c6134d89ca4526d1766edec7500dc1e
Hayoun Lee <sidhlee@gmail.com>
2020-07-03 12:58:59 +0900

    7. Animated Nav
    Toggle navigation with ternary style value with react spring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSpring, animated } from 'react-spring';
 import logo from './logo.svg';
 import './App.css';
 
 import Toggle from './Toggle';
+import Nav from './Nav';
 
 const App = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const navAnimation = useSpring({
+    transform: isNavOpen
+      ? `translate3d(0,0,0) scale(1)`
+      : // menu will appear from the top-left corner
+        `translate3d(-100%,-100%,0) scale(0.6)`,
+  });
   // You can take the 'to' values out to the top-level
   // and react-spring will understand that it's the 'to' value.
   const fade = useSpring({ from: { opacity: 0 }, opacity: 1 });
@@ -19,7 +27,13 @@ const App = () => {
     <animated.div className="App" style={fade}>
       <header className="App-header">
         <img src={logo} className="logo" alt="logo" />
-        <button className="menu-button">Menu</button>
+        <button
+          onClick={() => setIsNavOpen(!isNavOpen)}
+          className="menu-button"
+        >
+          Menu
+        </button>
+        <Nav style={navAnimation} />
       </header>
       <main>
         <Toggle />
