6edc9ea97376d927745db90a0b896e7ac5b37b0c
Hayoun Lee <sidhlee@gmail.com>
2020-07-06 20:46:03 +0900

    18. Gestures with Events
    Use events and interpolate to create complex gesture-based UI

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Animating React
 
 Code-along repo for the tutorial: [Animating React](https://www.leveluptutorials.com/tutorials/animating-react/)
+
+## React Use Gesture
+
+A set of hooks that let you bind mouse/touch events to React component.
+
+### Gesture state
+
+The hooks will call the provided handler passing a gesture state that includes the source event and other attributes.
+
+- [docs](https://use-gesture.netlify.app/docs/state)
--- src/Gesture.js
+++ src/Gesture.js
@@ -1,19 +1,35 @@
 import React from 'react';
 // lets you bind richer mouse and touch events to any component or view
 import { useGesture } from 'react-with-gesture';
-import { animated, useSpring, config } from 'react-spring';
+import { animated, useSpring } from 'react-spring';
 
 const Gesture = () => {
   // pass a function to useSpring to return an array with animation and set (and stop)
-  const [{ xy }, set] = useSpring(() => ({
-    xy: [0, 0],
-    config: config.stiff,
+  const [{ x }, set] = useSpring(() => ({
+    x: 0,
+    tension: 500,
+    friction: 4,
+    mass: 30,
   }));
 
   // destructure only the values we need (down & delta)
   const bind = useGesture(({ down, delta }) => {
     // set animation's xy value as delta(:how far cursor traveled in x and y axis )only when mouse is down
-    set({ xy: down ? delta : [0, 0] });
+    // set({ x: down ? delta[0] : 0 });
+
+    // You can implement complex logic to interact with gesture
+    if (down) {
+      set({ x: delta[0] });
+    } else {
+      // if swiped more than 300px left/right, make it disappear
+      if (delta[0] > 300) {
+        set({ x: 400 });
+      } else if (delta[0] < -300) {
+        set({ x: -400 });
+      } else {
+        set({ x: 0 });
+      }
+    }
   });
 
   return (
@@ -21,8 +37,14 @@ const Gesture = () => {
       // Call bind returned from useGesture and spread it into animated component
       {...bind()}
       style={{
+        // Scale x-delta into opacity
+        opacity: x.interpolate({
+          map: Math.abs, // map function to process input value
+          range: [0, 400], //  x-delta (px)
+          output: [1, 0], // opacity
+        }),
         // animation.xy passes x and y to interpolate
-        transform: xy.interpolate((x, y) => `translate3d(${x}px, ${y}px, 0)`),
+        transform: x.interpolate((x) => `translate3d(${x}px, 0, 0)`),
       }}
       className="box"
     ></animated.div>
